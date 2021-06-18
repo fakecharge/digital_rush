852dd9c68eec9ea70f46411ec6fd2ddbca15e2d1
lavApril13 <53113014+lavApril13@users.noreply.github.com>
2021-06-19 01:34:55 +0300

    статистика по отдельному пациенту

--- web_app/web_app.py
+++ web_app/web_app.py
@@ -19,6 +19,9 @@ def run_web_app(data_frame):
     df_patient = get_patient_data(data_frame, option)
     st.write(df_patient)
 
+    df_stat = get_patient_stat(df_patient)
+    st.write(df_stat)
+
     fig = plt.figure()
     ax = fig.add_subplot(1, 1, 1)
     ax.plot(df_patient['timedata'], df_patient['pulse'])
@@ -26,6 +29,13 @@ def run_web_app(data_frame):
     ax.set_xlabel("время")
     ax.set_ylabel("уд/мин")
     st.pyplot(fig)
+
+
+def get_patient_stat(df_patient):
+    df_stat = df_patient.describe()
+    df_stat = df_stat.drop(df_stat.index[[0]])
+    df_stat = df_stat[["pulse", "dia", "sys"]]
+    return df_stat
 
 
 def get_patient_data(df_all_patients, id_patient):
