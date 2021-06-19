8d077f37c127a0c75b9ace38ace5118c8b1d74e6
Andrey Dmitriev <deseq97@mail.ru>
2021-06-19 07:37:01 +0300

    Added camera test

--- DR2021/app/src/main/java/com/innino/dr2021/EnterData.java
+++ DR2021/app/src/main/java/com/innino/dr2021/EnterData.java
@@ -2,13 +2,53 @@ package com.innino.dr2021;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class EnterData extends AppCompatActivity {
+    final int bmpHeight = 160;
+    final int bmpWidth = 160;
+    static final int CAMERA_CODE = 1;
+    Uri image_uri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enter_data);
+    }
+
+    public void take_photo(View view) {
+        ContentValues values = new ContentValues();
+        values.put(MediaStore.Images.Media.TITLE, "New Picture");
+        values.put(MediaStore.Images.Media.DESCRIPTION, "From the Camera");
+        image_uri = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+        //Camera intent
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, image_uri);
+        startActivityForResult(cameraIntent, 1001);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == Activity.RESULT_OK){
+            switch(requestCode){
+                case 1:
+                    Log.i("CameraCode",""+CAMERA_CODE);
+                    Bundle bundle = data.getExtras();
+                    Bitmap bmp = (Bitmap) bundle.get("data");
+                    Bitmap resized = Bitmap.createScaledBitmap(bmp, bmpWidth,bmpHeight, true);
+            }
+
+
+        }
     }
 }
